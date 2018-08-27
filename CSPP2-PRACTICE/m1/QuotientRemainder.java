60fe40706a1d850db8586900d20fcd33102fbb2b
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-27 19:30:42 +0530

    Activity 13

---
+++ CSPP2-PRACTICE/m1/QuotientRemainder.java
@@ -0,0 +1,21 @@
+/**
+ * Quotient and Remainder.java.
+ * @author Sahith.
+ */
+public final class QuotientRemainder {
+    /**
+     * Empty Constructor.
+     */
+    private QuotientRemainder() {
+        //I am not using this constructor
+    }
+    /**
+     * main function to print Quotient and Remainder.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        int a = 9;
+        int b = 5;
+        System.out.println(a / b + " " + a % b);
+    }
+}
