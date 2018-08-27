273d887de06fbf63d28961fc622988a0965ccd7d
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-27 20:02:29 +0530

    Assignment-1

---
+++ CSPP2-PRACTICE/m1/Assignment-1/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * Solution.java.
+ * @author Sahith.
+ */
+public final class Solution {
+    /**
+     * Empty Constructor.
+     */
+    private Solution() {
+        //I am not using this constructor
+    }
+    /**
+     * main function to print name and roll number.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        String name = "Sahith";
+        int roll = 8;
+        System.out.println("Name :" + name + "Roll Number : " + roll);
+    }
+}
