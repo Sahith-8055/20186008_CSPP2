415eebfd6d84d77f477c1d2440df04e840faaa97
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-27 18:40:27 +0530

    CSPP2-PRACTICE

---
+++ CSPP2-PRACTICE/m1/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Hello World.java.
+ * @author Sahith.
+ */
+public final class HelloWorld {
+    /**
+     * Empty Constructor.
+     */
+    private HelloWorld() {
+        //I am not using this constructor
+    }
+    /**
+     * main function to print hello world.
+     * @param args is the parameter for this function
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World !!");
+    }
+}
