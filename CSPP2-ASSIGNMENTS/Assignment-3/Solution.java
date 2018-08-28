ccf0f5b8345a7a622db6d2a5576e3221425be6b6
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-28 16:24:50 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

---
+++ CSPP2-ASSIGNMENTS/Assignment-3/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        int result=power(base,exponent);
+        System.out.println(result);
+	}
+	public static int power(int base, int exponent) {
+		if (base == 0) {
+			return 0;
+		} else if(exponent == 0) {
+			return 1;
+		} else{
+			return base * (power(base, exponent - 1));
+		}
+	}
+}
+
