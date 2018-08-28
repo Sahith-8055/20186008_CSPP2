9301440e9357819308d1e34eec097af3353352b1
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-28 14:38:42 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ CSPP2-ASSIGNMENTS/Assignment-2/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		//rootsOfQuadraticEquation(a, b, c);
+		System.out.println(a + " " + b);
+	}
+	/*
+	Need to write the rootsOfQuadraticEquation function and print the output.
+	*/
+}
