981aefcab7a5f13987ae2bc8475a3c99fe86e282
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-31 13:40:38 +0530

    "check_style_score -> 0 of 1 passed. style score: 1/1 "

---
+++ CSPP2-ASSIGNMENTS/m4/Assignment-3/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty Constructor.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            String s = sc.next();
+            long res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { String }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    public static long binaryToDecimal(final String s) {
+        int base = 2;
+        int sum = 0;
+        int k = s.length() - 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                k = k - 1;
+            } else {
+                sum += Math.pow(base, k);
+                k = k - 1;
+            }
+        }
+        return sum;
+    }
+}
