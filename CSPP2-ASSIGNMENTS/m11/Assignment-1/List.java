de5c757403241d0da6516cda5e44fa7bb4fb0470
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-07 11:14:07 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m11/Assignment-1/List.java
+++ CSPP2-ASSIGNMENTS/m11/Assignment-1/List.java
@@ -273,8 +273,8 @@ public final class List {
         }
     }
     /*
-     Removes all of its elements that are contained in the specified int 
-     array.    
+     Removes all of its elements that are contained in the specified int
+     array.
     */
 
      /**
@@ -431,7 +431,9 @@ public final class List {
                     }
                 break;
                 case "subList": {
-                    if (tokens.length != 2)  break;
+                    if (tokens.length != 2) {
+                        break;
+                    }
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(Integer.parseInt(arrstring3[0]),
                             Integer.parseInt(arrstring3[1]));
