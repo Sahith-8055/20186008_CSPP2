8f47c960de505bdcd9c7010910b2cfd8b016849e
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-07 10:50:32 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m11/Assignment-1/List.java
+++ CSPP2-ASSIGNMENTS/m11/Assignment-1/List.java
@@ -161,6 +161,9 @@ public class List {
      * the list and not all the elements of the array.
      */
     public String toString() {
+    	if (size == 0) {
+    		return "[]";
+    	}
         String str = "[";
         int i = 0;
         for (i = 0; i < size - 1; i++) {
