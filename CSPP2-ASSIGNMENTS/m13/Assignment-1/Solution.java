6481b8c973cac3f3c0dae7423280a2035151e7d0
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-08 12:24:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m13/Assignment-1/Solution.java
+++ CSPP2-ASSIGNMENTS/m13/Assignment-1/Solution.java
@@ -161,7 +161,7 @@ class Set {
     public int[][] cartesianProduct(final Set list1) {
         int count = 0;
         int[][] set3 = new int[size * list1.size][2];
-        if (size != 0) {
+        if (size != 0 && list1.size != 0) {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < list1.size; j++) {
                     set3[count][0] = get(i);
