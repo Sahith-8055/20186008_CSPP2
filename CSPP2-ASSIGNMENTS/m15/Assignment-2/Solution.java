7dd7f4e81c4f8974e8706f23918e2379feac5e13
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-11 13:25:11 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m15/Assignment-2/Solution.java
+++ CSPP2-ASSIGNMENTS/m15/Assignment-2/Solution.java
@@ -97,7 +97,7 @@ final class SortedSet extends Set {
     public int[] headSet(final int toElement) throws SetEmptyException {
         int temp = 0;
         int[] result = new int[size];
-        if (size > 0) {
+        if (result.length > 0) {
         for (int i = 0; i < size(); i++) {
             if (set[i] < toElement) {
                 result[i] = set[i];
