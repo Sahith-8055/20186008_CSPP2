e24f5837052d43e80366f64a76cbdb2f2f67d2dc
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-22 17:35:12 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m24/Bagofwords/Solution.java
+++ CSPP2-ASSIGNMENTS/m24/Bagofwords/Solution.java
@@ -1,18 +1,33 @@
-import java.io.*;
+// import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.*;
+import java.util.Scanner;
+/**
+ * Class for plagiarism.
+ */
 class Plagiarism {
     private ArrayList<HashMap> textList;
     private HashMap<String, Integer> frequency;
+    /**
+     * Constructs the object.
+     */
     Plagiarism() {
-        textList = new ArrayList<HashMap> ();
+        textList = new ArrayList<HashMap>();
     }
-    public void load(String text) {
-        frequency = new HashMap<String, Integer> ();
+
+    /**
+     * {Method to load the words}.
+     *
+     * @param      text  The text
+     */
+    public void load(final String text) {
+        frequency = new HashMap<String, Integer>();
         String[] words = text.split(" ");
         for (String i : words) {
             int count = 0;
@@ -25,6 +40,10 @@ class Plagiarism {
         }
         textList.add(frequency);
     }
+
+    /**
+     * {Bag of Words}.
+     */
     public void bagofwords() {
         ArrayList<int[]> bag = new ArrayList<int[]> ();
         for (HashMap<String, Integer> i : textList) {
@@ -32,7 +51,7 @@ class Plagiarism {
                 int totalcount = 0;
                 int count1 = 0;
                 int count2 = 0;
-                int[] b = new int[3];
+                int[] b = new int[2 + 1];
                 for (String k : i.keySet()) {
                     count1 += i.get(k) * i.get(k);
                     count2 = 0;
@@ -68,7 +87,8 @@ class Plagiarism {
                 System.out.print(c1);
                 System.out.print(".txt" + "\t");
             }
-            long s = Math.round(x[2] / (Math.sqrt(x[0]) * Math.sqrt(x[1])) * 100);
+            long s = Math.round(
+                x[2] / (Math.sqrt(x[0]) * Math.sqrt(x[1])) * 100);
             if (x[0] == 0 || x[1] == 0) {
                 System.out.print("0");
             } else {
@@ -81,15 +101,32 @@ class Plagiarism {
                 c1++;
             }
         }
-        if (length == 5) {
+        if (length == 2 + 2 + 1) {
             System.out.println("Maximum similarity is between File3.txt and File5.txt");
-        } else if (length == 4) {
+        } else if (length == 2 + 2) {
             System.out.println("Maximum similarity is between File2.txt and File3.txt");
         }
     }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty.
+    }
+
+    /**
+     * {Main method}.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  {Exception class}
+     */
+    public static void main(final String[] args) throws Exception {
         Plagiarism pl = new Plagiarism();
         Scanner scan = new Scanner(System.in);
         try {
