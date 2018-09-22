0b54bd4acc3393ad2cf90dc08bff6a404f5e3174
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-22 11:47:39 +0530

    "check_style_score -> 6 of 14 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m25/Assignment-1/TodoistMain.java
+++ CSPP2-ASSIGNMENTS/m25/Assignment-1/TodoistMain.java
@@ -235,20 +235,11 @@ public class TodoistMain {
      */
     public static Task createTask(final String[] tokens) throws Exception {
         String title = tokens[1];
-        if (title.length() == 0) {
-            throw new Exception ("Title not provided");
-        }
         String assignedTo = tokens[2];
         int timeToComplete = Integer.parseInt(tokens[3]);
-        if (timeToComplete < 0) {
-            throw new Exception ("Invalid timeToComplete" + " " + timeToComplete);
-        }
         boolean important = tokens[4].equals("y");
         boolean urgent = tokens[5].equals("y");
         String status = tokens[6];
-        if (!(status.equals("todo")) || (status.equals("done"))) {
-            throw new Exception ("Invalid status" + " " + status);
-        }
         return new Task(
             title, assignedTo, timeToComplete, important, urgent, status);
     }
