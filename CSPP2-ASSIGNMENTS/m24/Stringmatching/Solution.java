7b283bd60dd7ec9690cc00a1487d86c5d83c47f9
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-09-22 18:32:33 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- CSPP2-ASSIGNMENTS/m24/Stringmatching/Solution.java
+++ CSPP2-ASSIGNMENTS/m24/Stringmatching/Solution.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,23 +14,24 @@ class CommonString {
     /**
      * Constructs the object.
      *
-     * @param      str1  The 1st String
-     * @param      str2  The 2nd String
+     * @param      str1  The 1st string
+     * @param      str2  The 2nd string
      */
     CommonString(final String str1, final String str2) {
-        this.file1 = str1;
-        this.file2 = str2;
+        file1 = str1;
+        file2 = str2;
     }
+
     /**
-     * {Method to find the longest common substring}
+     * {Method for finding the common substring}.
      *
-     * @param      arr1  The arr 1
-     * @param      arr2  The arr 2
-     * @param      m     {variable m}
-     * @param      n     {variable n}
+     * @param      arr1     { parameter_description }
+     * @param      arr2     { parameter_description }
+     * @param      m     { parameter_description }
+     * @param      n     { parameter_description }
      */
-    public void common(final char[] arr1, final char[] arr2,
-        final int m, final int n) {
+    public void common(final char[] arr1,
+        final char[] arr2, final int m, final int n) {
         Double[][] longest = new Double[m + 1][n + 1];
         result = 0.0;
         for (int i = 0; i <= m; i++) {
@@ -39,9 +41,8 @@ class CommonString {
                 } else if (arr1[i - 1] == arr2[j - 1]) {
                     longest[i][j] = longest[i - 1][j - 1] + 1;
                     result = Double.max(result, longest[i][j]);
-                } else {
+                } else
                     longest[i][j] = 0.0;
-                }
             }
         }
         res = (double)Math.round((((result * 2) / (m + n)) * 100d));
@@ -53,7 +54,7 @@ class CommonString {
 /**
  * Class for solution.
  */
-public final class Solution {
+class Solution {
     /**
      * Constructs the object.
      */
@@ -66,25 +67,23 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        File testcase = new File(scan.nextLine());
+        Scanner input = new Scanner(System.in);
+        try {
+        File testcase = new File(input.nextLine());
         File[] testfiles = testcase.listFiles();
-        if (testfiles.length == 0) {
-            System.out.println("Empty directory");
-        } else {
+
             System.out.print("             ");
             for (File name : testfiles) {
-                System.out.print(
-                    name.toString().split("\\\\")[1] + "    ");
+                System.out.print(name.toString().split("\\\\")[1] + "    ");
             }
             ArrayList<String> fileStrings = new ArrayList<>();
             ArrayList<CommonString> objects = new ArrayList<>();
             for (File filename : testfiles) {
                 try {
-                    String content = new String(
-                        Files.readAllBytes(Paths.get(filename.getAbsolutePath())));
-                    content = content.replaceAll("\n", " ").replaceAll(
-                        "[^A-Za-z0-9 ]", "").replaceAll("\\s+", " ");
+                    String content = new String(Files.readAllBytes(
+                        Paths.get(filename.getAbsolutePath())));
+                    content = content.replaceAll("\n", " ")
+                    .replaceAll("[^A-Za-z0-9 ]", "").replaceAll("\\s+", " ");
                     fileStrings.add(content);
                 } catch (Exception e) {
                     System.out.println("file not found");
@@ -97,13 +96,16 @@ public final class Solution {
                 System.out.print("File" + i + ".txt" + "        ");
                 for (String input2 : fileStrings) {
                     objects.add(new CommonString(input1, input2));
-                    objects.get(k).common(
-                        input1.toCharArray(), input2.toCharArray(), input1.length(), input2.length());
+                    objects.get(k).common(input1.toCharArray(),
+                     input2.toCharArray(),input1.length(), input2.length());
                     System.out.print(objects.get(k).getResult() + "         ");
                     k++;
                 }
                 i++;
             }
+        } catch(Exception e){
+            System.out.println("Empty Directory");
         }
     }
+
 }
